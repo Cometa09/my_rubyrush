f0e32718f32731aa08281479d63d5d6da13091ba
Cometa09 <sokolovalex09@gmail.com>
2022-10-27 17:13:37 +0300

    Finish Step 104 of 149. Add ProductCollection.

--- lesson23/shop/main.rb
+++ lesson23/shop/main.rb
@@ -1,12 +1,19 @@
 require_relative 'lib/product'
 require_relative 'lib/book'
 require_relative 'lib/movie'
+require_relative 'lib/product_collection'
 
-current_path = File.dirname(__FILE__)
-book = Book.from_file(current_path + '/data/books/01.txt')
+# Создаем коллекцию продуктов, передавая методу класса from_dir путь к папке
+# с подпапками films и books. ProductCollection сам знает, как там внутри лежат
+# эти файлы и сам разбереться, как их оттуда считать.
+collection = ProductCollection.from_dir(File.dirname(__FILE__) + '/data')
 
-movie = Movie.from_file(current_path + '/data/films/01.txt')
+# Сортируем продукты по возрастанию цены с помощью метода sort! экземпляра
+# класса ProductCollection
+collection.sort!(by: :price, order: :asc)
 
-puts book
-movie.update(price: 2999)
-puts movie
+# Получаем массив продуктов методом to_a и выводим каждый на экран, передавая
+# его методу puts в качестве аргумента.
+collection.to_a.each do |product|
+  puts product
+end
